5d875416e3a102c61411d3cee3609762c050c610
tanvirwebtech <tanvirahmed9593@gmail.com>
2023-08-14 20:48:38 +0600

    cors config chng2

--- index.js
+++ index.js
@@ -15,7 +15,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // MIDDLEWARE
-app.use(cors());
+app.use(
+    cors({
+        credentials: true,
+        origin: `${process.env.CLIENT_URL}`,
+    })
+);
 app.use(express.json());
 app.use(cookieParser());
 
